c7900bea4acac814c68b931be2f751c44db855ad
Sam Leifer <samleifer@Sams-Air.lan>
2025-01-19 11:22:09 -0600

    fixes

--- adopters/views.py
+++ adopters/views.py
@@ -56,7 +56,6 @@ class AdopterViewSet(viewsets.ModelViewSet):
             {
                 "adopter": AdopterSerializer(adopter).data,
                 "currentAppointment": AppointmentSerializer(appointment).data if appointment else None,
-                # "bookings": [BookingSerializer(booking).data for booking in adopter.bookings],
                 "bookingHistory": adopter.booking_history
             }
         )  
