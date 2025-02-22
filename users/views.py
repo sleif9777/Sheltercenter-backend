e78cb40609ab21aac5d2771f814ca94f009696fd
Sam Leifer <samleifer@sams-air.lan>
2025-02-21 18:00:33 -0600

    Expose codes in login

--- users/views.py
+++ users/views.py
@@ -39,10 +39,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
                 user.reset_otp()
 
                 return JsonResponse({
-                    "message": "New one-time passcode sent to your email."
+                    "message": "Your one-time passcode is: " + user.otp
                 }, status=status.HTTP_202_ACCEPTED)
             except Exception as e:
-                print(e)
                 traceback.print_exc()
                 return JsonResponse({
                     "message": "Your one-time passcode is: " + user.otp
